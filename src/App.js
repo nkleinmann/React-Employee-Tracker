acfba28446146698b3b8860f3436ef11aebb8053
nkleinmann <nekleinmann@gmail.com>
2020-10-31 18:05:10 -0400

    added utils folder and API.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import SortNames from "./components/sort/Sort";
+import { API } from './utils/API';
+// import SortNames from "./components/sort/Sort";
+
+function SortNames(employee) {
+  console.log("This worked!");
+  employee.sort();
+  console.log(`This is the sorted version ${employee}`);
+  // resource: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+
+
+  // const myData = [].concat(this.state.employees)
+  // .sort((a, b) => a.itemM > b.itemM ? 1: -1)
+  // .map((item, i) =>
+  // <div key={i}> {item.matchID} {item.timeM}{item.description} </div>
+  // );
+  // https://stackoverflow.com/questions/43572436/sort-an-array-of-objects-in-react-and-render-them/43572944
+}
 
 // class component
 class App extends Component {
@@ -12,8 +28,11 @@ class App extends Component {
   }
   // api calls in this function (life cycle method)
   componentDidMount() {
-    axios.get("https://randomuser.me/api/?results=200&nat=us").then((response) => {
-      this.setState({ employees: response.data.results });
+    API.getEmployees()
+    .then((response) => {
+      this.setState({ 
+        employees: response.data.results 
+      });
     });
   }
   render() {
--- src/components/sort/Sort.js
+++ src/components/sort/Sort.js
@@ -1,16 +1,19 @@
 import React, { useRef } from "react";
+import App from './App';
 
-function SortNames() {
-    console.log("This worked!");
-    // resource: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
+// function SortNames(employee) {
+//     console.log("This worked!");
+//     employee.sort();
+//     console.log(`This is the sorted version ${employee}`);
+//     // resource: https://www.sitepoint.com/sort-an-array-of-objects-in-javascript/
 
 
-    // const myData = [].concat(this.state.employees)
-    // .sort((a, b) => a.itemM > b.itemM ? 1: -1)
-    // .map((item, i) =>
-    // <div key={i}> {item.matchID} {item.timeM}{item.description} </div>
-    // );
-    // https://stackoverflow.com/questions/43572436/sort-an-array-of-objects-in-react-and-render-them/43572944
-}
+//     // const myData = [].concat(this.state.employees)
+//     // .sort((a, b) => a.itemM > b.itemM ? 1: -1)
+//     // .map((item, i) =>
+//     // <div key={i}> {item.matchID} {item.timeM}{item.description} </div>
+//     // );
+//     // https://stackoverflow.com/questions/43572436/sort-an-array-of-objects-in-react-and-render-them/43572944
+// }
 
-export default SortNames;
+// export default SortNames;
